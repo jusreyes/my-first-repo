e3aa51abd13f6c7c788ddb5b072a2b0f98ec8940
jusreyes <jusreyes@ucsc.edu>
2020-04-22 23:48:21 -0500

    function to compare value succesfull

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,4 +1,21 @@
 
 line_number=$(ls -1 | wc -l)
 
+echo " How many files are in the current directory ?"
+echo " Enter a guessed number of files"
 
+read g_number
+#echo "value of read number is $g_number"
+if [[ $g_number -eq $line_number ]]
+ then
+	 echo "You guessed right, Congratulaions !"
+ elif [[ $g_number -gt $line_number ]]
+ then
+	 echo "You guessed too high !"
+ else
+	 echo "you guessed too low !"
+ fi
+
+
+
+													                    
